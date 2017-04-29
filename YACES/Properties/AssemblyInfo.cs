7cf383e54c9aae586de6ae5bbe61281b576ccdbd
impliedfeline <kinnunen.jussi@hotmail.com>
2017-04-29 21:32:36 +0300

    Cleanup project

--- YACES/Properties/AssemblyInfo.cs
+++ YACES/Properties/AssemblyInfo.cs
@@ -1,9 +1,10 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-// Information about this assembly is defined by the following attributes. 
+// Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
-[assembly: AssemblyTitle ("YACESTest")]
+
+[assembly: AssemblyTitle ("YACES")]
 [assembly: AssemblyDescription ("")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
@@ -11,12 +12,16 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright ("juicyp")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
+
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion ("1.0.0")]
-// The following attributes are used to specify the signing key for the assembly, 
+
+[assembly: AssemblyVersion ("1.0.*")]
+
+// The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
+
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
 
